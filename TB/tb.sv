e91e184af4f64346d231484b34207621bad1034b
SamehM20 <134065615+SamehM20@users.noreply.github.com>
2024-06-04 22:00:04 +0300

    Update tb.sv

--- TB/tb.sv
+++ TB/tb.sv
@@ -12,7 +12,7 @@ module tb;
   always #10 clk = ~clk;
 
   // Monitoring Data Values when Valid
-  always @(posedge data_valid_o) $display("\n\n Data In: %h, \n Mode: %s, \n Key: %h, \n Encrypted Data: %h", data_old, mode_i.name, key_old, data_o);
+  always @(posedge data_valid_o) $display("Data In: %h, \n Mode: %s, \n Key: %h, \n Encrypted Data: %h \n\n", data_old, mode_i.name, key_old, data_o);
 	
   // Perserving added keys and data for monitoring
   always @(posedge ready_o) begin
@@ -35,34 +35,34 @@ initial begin
   @(posedge ready_o)
 
   #10;
-  $display("Case 2:");
   data_i = 128'h3243f6a8885a308d313198a2e0370734;
   key_i = {128'h2b7e151628aed2a6abf7158809cf4f3c, 128'b0};
   @(posedge ready_o)
+  $display("Case 2:");
 
   #10;
-  $display("Case 3:");
   data_i = 128'h00112233445566778899aabbccddeeff;
   key_i = {128'h000102030405060708090a0b0c0d0e0f, 128'b0};
   @(posedge ready_o)
+  $display("Case 3:");
   #10;
 
 
 
   $display("Testing ENC_192");
-  $display("Case 4:");
   mode_i = ENC_192;
   key_i = {192'h000102030405060708090a0b0c0d0e0f1011121314151617, 64'b0};	
   @(posedge ready_o)
+  $display("Case 4:");
   #10;
 
 
   $display("Testing ENC_256");
-  $display("Case 5:");
   mode_i = ENC_256;
   key_i = 256'h000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f;
   @(posedge ready_o)
-	#40;
+  $display("Case 5:");
+  #40;
   $finish();
 end
 endmodule
